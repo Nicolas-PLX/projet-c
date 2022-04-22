40d284bb93ada8cc2809005c3a7f87f74df97ed3
npeneloux <nicolas.peneloux@free.fr>
2022-04-22 09:48:02 +0200

    ajouts en tout genre

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -1,10 +1,32 @@
-#include <"unbounded_int.c">
+#include "unbounded_int.c"
 
 #include <stdio.h>
 #include <stdlib.h>
 
 
 
+//Fonction boolean qui regarde si l'option i est présente dans les arguments du main.
+//Renvoie 1 si elle y est, sinon 0
+int option_i(char *argv[]){
+    int i = 0;
+    while(argv[i] != "\0"){
+        if (argv[i] == "-i"){
+            return 1;
+        }
+    }
+    return 0;
+}
+//Fonction boolean qui regarde si l'option o est presente dans lesa rguments du main.
+//Renvoie 1 si elle y est, sinon 0
+int option_o(char *argv[]){
+    int i = 0;
+    while(argv[i] != "\0"){
+        if (argv[i] == "-o"){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 
@@ -12,6 +34,9 @@
 
 
 
-void main(){
+
+void main(int argc, char *argv[]){
+    int bool_i = option_i(argv);
+    int bool_o = option_o(argv);
 
 }
