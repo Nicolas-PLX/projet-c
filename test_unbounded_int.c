e2aee7757a577fdb46833e0c08b6dfebe3289616
Victoria Lim <83660724+Susumou@users.noreply.github.com>
2022-05-22 18:13:54 +0200

    Modif

--- test_unbounded_int.c
+++ test_unbounded_int.c
@@ -4,7 +4,7 @@ int main() {
 
     char str1[] = "600",
          str2[] = "9000",
-         str3[] = "0",
+         str3[] = "9",
          str4[] = "31";
     long long ll1 = 73,
               ll2 = 93,
